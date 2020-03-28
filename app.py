7d454f0f10a87fb4e8652e9942e6fb7538ce09ca
a2kad <55212679+a2kad@users.noreply.github.com>
2020-03-28 20:36:40 +0100

    Add Cases and Bar.Graph

--- app.py
+++ app.py
@@ -3,9 +3,19 @@ import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objects as go
 
-data = pd.read_csv('covid.csv')
-print(data.head())
+data_csv = pd.read_csv('covid.csv')
+#print(data_csv.head())
+last_day = data_csv.tail(1)
+confirme = last_day['Confirme']
+death = last_day['Death']
+hospitalized = last_day['Hospitalized']
+recovered = last_day['Recovered']
+all_date = data_csv['Date']
+all_confirme = data_csv['Confirme']
+
+fig = go.Figure(data=[go.Bar(x=all_date, y=all_confirme, text = 'Cas confirmés')], layout = {'title': 'Résultat positif sur COVID-19 (depuis le 24 février)'})
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -32,23 +42,31 @@ app.layout = html.Div([
     html.Div([
         html.Div([
             html.Div([
-                html.H2('586'),
-                html.P('Confirmed')
+                html.H2(confirme),
+                html.P('positives au COVID-19')
             ], className='overviewcard'),
             html.Div([
-                html.H2('586'),
-                html.P('Confirmed')
+                html.H2(death),
+                html.P('décès à l\'hôpital')
             ], className='overviewcard'),
             html.Div([
-                html.H2('586'),
-                html.P('Confirmed')
+                html.H2(hospitalized),
+                html.P('sont hospitalisées')
+            ], className='overviewcard'),
+            html.Div([
+                html.H2(recovered),
+                html.P('sont sorties de l\'hôpital')
             ], className='overviewcard')
         ], className='main-overview'),
         html.Div([
-            html.Div(['Map'
+            html.Div([
+                dcc.Graph(
+                    id = 'example-graph',
+                    figure=fig
 
+                )
             ], className='card'),
-            html.Div(['Card 1'
+            html.Div(['Card 1',
 
             ], className='card'),
             html.Div(['Card 2'
